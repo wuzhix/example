1b2557aba7925b373e01ceb51b5a6233b3fb0c93
wuzx <wuzhixiang1984@gmail.com>
2019-07-12 16:10:04 +0800

    增加for循环的使用陷阱

---
+++ src/demo/trap/for.go
@@ -0,0 +1,20 @@
+package trap
+
+import "time"
+
+func Foreach()  {
+	println("直接使用参数")
+	for i := 0; i < 5; i++ {
+		go func() {
+			println(i)
+		}()
+	}
+	time.Sleep(3 * time.Second)
+	println("传入参数")
+	for i := 0; i < 5; i++ {
+		go func(index int) {
+			println(index)
+		}(i)
+	}
+	time.Sleep(3 * time.Second)
+}
